4f8411f1b9bbadaa297d08af7770e14c6e8621c3
Sigalsha <sigal.shalit.manor@gmail.com>
2021-04-05 18:39:19 +0300

    fix gh-pages routing issue

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { Fragment } from "react";
-import { BrowserRouter as Router, Route } from "react-router-dom";
+import { HashRouter as Router, Route } from "react-router-dom";
 import MenuContextProvider from "./contexts/MenuContext";
 import { GlobalStyles } from "./styles/global";
 import Landing from "./components/routeComponents/Landing";
